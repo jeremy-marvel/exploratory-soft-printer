b9c3f65efba31275cde29fedb320f57e2b5a23e0
jcase-robots <jennifer.case@nist.gov>
2022-09-23 12:25:39 -0400

    Adjust default max feedrate and acceleration

--- marlin-firmware/Marlin/Configuration.h
+++ marlin-firmware/Marlin/Configuration.h
@@ -1162,7 +1162,7 @@
   #define Z_FEEDRATE   25
 #endif
 
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, Z_FEEDRATE, 40 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, Z_FEEDRATE, 10*60 } // adjust the default_max_feedrate from 40 to 10*60 - J. Case
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1175,7 +1175,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 9000, 9000, 100, 9000 }
+#define DEFAULT_MAX_ACCELERATION      { 9000, 9000, 100, 25 } // reduce default_max_accelration - J. Case
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
